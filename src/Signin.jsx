7976a70b854ffd86301474156eb58e48f8df2b99
regul4rn0name <92522298+regul4rn0name@users.noreply.github.com>
2025-03-26 17:16:17 +0100

    profile page

--- src/Signin.jsx
+++ src/Signin.jsx
@@ -31,6 +31,7 @@ function Signin() {
     },[]);
     return (
         <div className=' absolute top-0 m-auto mt-3.5 h-screen w-screen bg-[rgb(29,29,29)] left-0 items-center justify-center'>
+             <button className=" fixed top-1 left-0 w-28 ml-1.5 p-2.5 rounded-3xl cursor-pointer text-[16px] text-white items-center justify-center flex h-[75px] bg-[#333] hover:border-[3px] duration-25 hover:border-white m-auto" onClick={()=>navigate('/')}>Main Menu</button>
             {!profile ? (<> <div className='bg-[#1E1E1E] p-8 rounded-[45px] shadow-[0_0_10px_rgba(255,255,255,0.1)] w-[550px] text-center m-auto mt-[50px] mb-[50px] h-fit'>
                 <h2 className="mb-2.5">Sign In</h2>
                 <p className="text-sm mb-5">Choose a sign-in method below.</p>
@@ -39,8 +40,18 @@ function Signin() {
                     <button class="w-[35%] p-2.5 m-[10px] rounded-3xl cursor-pointer text-[16px] text-white items-center justify-center inline-flex h-[75px] bg-[#333] hover:border-[3px] duration-25 hover:border-white" onClick={logingoogle}><img className='h-[45px] rounded-full' src='google.webp' />Sign in with Google</button>
                 </div>
             </div>
-            <button className=" w-[150px] p-2.5 rounded-3xl cursor-pointer text-[16px] text-white items-center justify-center flex h-[75px] bg-[#333] hover:border-[3px] duration-25 hover:border-white m-auto" onClick={()=>navigate('/')}>Main Menu</button></>):(<><h3>Welcome, {profile.name}</h3>
-                <p>id: {profile.id}</p></>)}
+          </>):
+            (
+                <>
+                <div className='bg-[#1E1E1E] p-8 rounded-[45px] shadow-[0_0_10px_rgba(255,255,255,0.1)] w-[80vw] text-center m-auto mt-[50px] mb-[50px] h-fit'>
+                <h2 className="mb-2.5">Welcome {profile.name}!</h2>
+                <p className="text-sm mb-5">You can check your statiscs below</p>
+                
+                
+            </div>
+           
+                </>
+            )}
            
         </div>
     );
