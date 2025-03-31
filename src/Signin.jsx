979b1321dc030cade3837efea7a8d58af69a1d5d
regul4rn0name <92522298+regul4rn0name@users.noreply.github.com>
2025-03-31 17:40:25 +0200

    Relogin, recreate auth token

--- src/Signin.jsx
+++ src/Signin.jsx
@@ -3,26 +3,34 @@ import Login from "./Login";
 import Profile from "./Profile";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Cookies from 'js-cookie';
 
 function Signin() {
     const navigate = useNavigate();
     const [profile, setProfile] = useState(null);
+    const token = Cookies.get('token');
+    const reftoken = Cookies.get('reftoken');
 
     const fetchProfile = async () => {
-        try {
-            const res = await axios.get('http://localhost:3001/profile', { withCredentials: true });
-            setProfile(res.data.user);
-            console.log(profile);
-
-
-        } catch (error) {
-            console.error(error);
-
+        if(token!=null){
+            try {
+                const res = await axios.get('http://localhost:3001/profile', { withCredentials: true });
+                setProfile(res.data.user);
+                console.log(profile);
+    
+    
+            } catch (error) {
+                console.error(error);
+    
+            }
+        }else if(reftoken!=null){
+            axios.post('http://localhost:3002/token',{},{withCredentials:true});
         }
+        
     };
     useEffect(() => {
         fetchProfile();
-    }, []);
+    }, [token]);
 
     return (
         <div className='m-auto min-h-screen h-fit w-screen bg-[rgb(29,29,29)] items-center justify-center bg-gradient-to-b from-stone-900 to-neutral-700'>
