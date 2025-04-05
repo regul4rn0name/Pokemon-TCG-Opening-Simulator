b4f21b7ba894851f4d1e2d42a022d2bf90134022
regul4rn0name <92522298+regul4rn0name@users.noreply.github.com>
2025-04-05 20:01:55 +0200

    shadows :)

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -8,7 +8,7 @@ export default function Profile({ profile }) {
             <p className="text-sm mb-10">You can check your statiscs below</p>
             <div className="grid grid-cols-4 gap-4 items">
                 {Object.entries(profile.CardsAmount).map(([rarity,amount])=>(
-                    <div key={rarity} className="p-4 border-2 rounded-2xl text-left">
+                    <div key={rarity} className="p-4 border-2 rounded-2xl text-left  shadow-lg shadow-stone-700">
                         <p>{rarity}</p>
                         <p>{amount}</p>
                     </div>
