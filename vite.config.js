8f26f6c837a75d68daf60bd11ec3299e9af59b87
regul4rn0name <92522298+regul4rn0name@users.noreply.github.com>
2025-04-16 23:46:09 +0200

    config2

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   base: '/',
   plugins: [tailwindcss(),react()],
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript'
+    }
+  }
 })
