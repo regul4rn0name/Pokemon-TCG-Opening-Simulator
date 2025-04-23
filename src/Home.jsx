5a2713084c59828779667f49a9e9f7a42aebc26e
regul4rn0name <92522298+regul4rn0name@users.noreply.github.com>
2025-04-23 21:19:14 +0200

    up and down button

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { setupCache } from 'axios-cache-interceptor';
 import Axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,useRef } from 'react';
 import Header from './Header';
 import HomeLoading from './HomeLoading';
 
@@ -10,15 +10,17 @@ const instance = Axios.create();
 const axios = setupCache(instance);
 
 function Home() {
-
+  const scrollref = useRef(null);
+  const windowRef = useRef(null);
   const [matches, setMatches] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [up,setUp] = useState(true);
     useEffect(() => {
       setLoading(true);
       setError(null);
      const controller = new AbortController();
-      axios.get(`https://pokemon-tcg-opening-simulator.jajca.site/server/table`, { signal:controller.signal }).then(response => {
+      axios.get(`http://localhost:3001/table`, { signal:controller.signal }).then(response => {
       
           if (response.data ) {
             setMatches(response.data.rows)
@@ -56,14 +58,47 @@ function Home() {
     const handleRedirect = (table)=>{
         navigate("/"+table);
     }
-
+    const scroll = ()=>{
+      if (up) {
+        windowRef.current.scrollTo({
+          top: windowRef.current.scrollHeight,
+          behavior: "smooth"
+        });
+      }else{
+        windowRef.current.scrollTo({
+          top:0,
+          behavior:"smooth"
+        })
+      }
+    }
+    const handlescroll=(e)=>{
+      const {scrollTop,scrollHeight,clientHeight} = e.target;
+      const scrollRatio = scrollTop/(scrollHeight-clientHeight);
+      if(scrollRatio<=0.5 && !up){
+        setUp(true)
+        
+      }else if(up && scrollRatio>0.5){
+        setUp(false)
+      }
+      console.log(up);
+      
+    }
+    useEffect(()=>{
+      if (scrollref.current) {
+        scrollref.current.style.transform = up ? "rotate(0deg)" : "rotate(180deg)";
+      }
+      
+    },[up]);
     return (
-      <div id="jajca" className='flex flex-wrap justify-center h-screen overflow-y-auto flex-grow text-white gap-x-5 gap-y-5 text-center bg-[rgb(29,29,29)] '>
+      <div id="jajca" ref={windowRef} className='flex flex-wrap justify-center h-screen overflow-y-auto flex-grow text-white gap-x-5 gap-y-5 text-center bg-[rgb(29,29,29)] ' onScroll={handlescroll}>
         {loading && <HomeLoading/>}
         {error && <div>Error: {error.message}</div>}
-        {!loading && !error &&<Header/>}
+        {!loading && !error &&<Header/> && (
+          <div className='bg-[rgb(77,76,76)] h-11 w-11 flex justify-end fixed bottom-4 right-4 rounded-full'>
+            <button className='rounded-full bg-[rgb(77,76,76)] h-11 w-11  hover:border-white hover:border-[3px]  duration-25 rotate-180' ref={scrollref} onClick={scroll}>&uarr;</button>
+          </div>
+        )}
         {!loading && !error  && (
-          
           matches.map((table) => (
             <div key={table.name}>
               <button className='md:w-[456px] md:h-[256px] mt-5  w-[456px] h-[256px] sm:h-[128px] sm:w-[233px] bg-no-repeat bg-contain bg-center bg-[rgb(22,34,73)] shadow-none border-[3px] border-[rgb(22,34,73)] rounded-2xl duration-25 hover:border-[#646cff]' 
